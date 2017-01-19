f6ddfe66c55352db6ce3e7c1a148860bce0b6b9a
rsavian <rsavian@nerdery.com>
2017-01-19 11:07:18 -0600

    add subDependencies

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,10 +1,18 @@
 require('babel-register')({ ignore: /node_modules|tools\/cache/ });
 require('browser-sync').create('local');
 require('env-merger')();
-
 const fs = require('fs');
 const gulp = require('gulp');
+const pkg = require('./package.json');
 const tasksDir = './tasks';
+
+// Used if we only want to bring parts of a library into vendor.js
+Object.keys(pkg.subDependencies).forEach(dep => {
+    delete pkg.dependencies[dep];
+    pkg.subDependencies[dep].forEach(subdep => {
+        pkg.dependencies[subdep] = '0.0.0';
+    });
+});
 
 // Lazy-load all gulp tasks found under /tasks
 fs.readdirSync(tasksDir).forEach(filename => {
@@ -20,4 +28,4 @@ fs.readdirSync(tasksDir).forEach(filename => {
 
 gulp.task('default', ['build']);
 
-console.log(`BUILDING USING ENVIRONMENT: ${process.env.NODE_ENV || 'default'}`);
+console.log(`BUILDING USING ENVIRONMENT: ${process.env.NODE_ENV || 'production'}`);
